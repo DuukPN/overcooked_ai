ed7de89c2bd22a467c31cda551f8d45464043f6b
Duuk Niemantsverdriet <D.P.Niemantsverdriet@student.tudelft.nl>
2023-06-02 12:11:08 +0200

    fix setup

--- job.sh
+++ job.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name="bc_experiment"
 #SBATCH --partition=compute
 #SBATCH --time=01:30:00
-#SBATCH --ntasks=10
+#SBATCH --ntasks=2
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=30GB
 #SBATCH --account=Education-EEMCS-Courses-CSE3000
