0c1ce68aa5a9fab09186d7e4e72a85d95ac0e51d
Duuk Niemantsverdriet <D.P.Niemantsverdriet@student.tudelft.nl>
2023-06-02 08:04:09 +0200

    more memory

--- job.sh
+++ job.sh
@@ -2,10 +2,10 @@
 #
 #SBATCH --job-name="bc_experiment"
 #SBATCH --partition=compute
-#SBATCH --time=10:00:00
+#SBATCH --time=02:30:00
 #SBATCH --ntasks=10
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=8GB
+#SBATCH --mem-per-cpu=18GB
 #SBATCH --account=Education-EEMCS-Courses-CSE3000
 
 module load 2022r2
