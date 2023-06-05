36152ca1b45ced8af4e8ef6d3882bd073ed51953
Duuk Niemantsverdriet <D.P.Niemantsverdriet@student.tudelft.nl>
2023-06-05 22:17:31 +0200

    run second thingy

--- job.sh
+++ job.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name="bc_experiment"
 #SBATCH --partition=compute
 #SBATCH --time=01:00:00
-#SBATCH --ntasks=2
+#SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=48GB
 #SBATCH --account=Education-EEMCS-Courses-CSE3000
@@ -14,5 +14,5 @@ module load miniconda3
 conda deactivate
 conda activate /home/dniemantsverdr/env
 
-srun python ~/overcooked_ai/src/human_aware_rl/imitation/my_experiments.py "$1"
+#srun python ~/overcooked_ai/src/human_aware_rl/imitation/my_experiments.py "$1"
 srun python ~/overcooked_ai/src/human_aware_rl/imitation/my_experiments.py "$1" -s
