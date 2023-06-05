3faed573d02802c2cb29fa71c85b875a90fed470
Duuk Niemantsverdriet <D.P.Niemantsverdriet@student.tudelft.nl>
2023-06-05 14:07:36 +0200

    fix very minor bug

--- job.sh
+++ job.sh
@@ -2,10 +2,10 @@
 #
 #SBATCH --job-name="bc_experiment"
 #SBATCH --partition=compute
-#SBATCH --time=01:30:00
+#SBATCH --time=01:00:00
 #SBATCH --ntasks=2
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=50GB
+#SBATCH --mem-per-cpu=48GB
 #SBATCH --account=Education-EEMCS-Courses-CSE3000
 
 module load 2022r2
