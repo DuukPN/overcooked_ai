bc476037ce2ec502f905de2535cabbbce89f6acb
Duuk Niemantsverdriet <D.P.Niemantsverdriet@student.tudelft.nl>
2023-06-13 08:07:59 +0200

    Stop violating QoS

--- job.sh
+++ job.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name="bc_experiment"
 #SBATCH --partition=compute
 #SBATCH --time=01:30:00
-#SBATCH --ntasks=10
+#SBATCH --ntasks=2
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=48GB
 #SBATCH --account=Education-EEMCS-Courses-CSE3000
@@ -14,8 +14,5 @@ module load miniconda3
 conda deactivate
 conda activate /home/dniemantsverdr/env
 
-for idx in "0" "1" "2" "3" "4"
-do
-  srun python ~/overcooked_ai/src/human_aware_rl/imitation/my_experiments.py "$1" $idx
-  srun python ~/overcooked_ai/src/human_aware_rl/imitation/my_experiments.py "$1" $idx -h
-done
+srun python ~/overcooked_ai/src/human_aware_rl/imitation/my_experiments.py "$1" "$2"
+srun python ~/overcooked_ai/src/human_aware_rl/imitation/my_experiments.py "$1" "$2" -h
