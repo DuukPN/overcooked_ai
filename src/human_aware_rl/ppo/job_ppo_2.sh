7c31bd82d2127f7dc5cefc45061805ea8383838b
Duuk Niemantsverdriet <D.P.Niemantsverdriet@student.tudelft.nl>
2023-06-13 17:41:15 +0200

    compute node

--- src/human_aware_rl/ppo/job_ppo_2.sh
+++ src/human_aware_rl/ppo/job_ppo_2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 #SBATCH --job-name="bc_experiment_train_ppo"
-#SBATCH --partition=memory
+#SBATCH --partition=compute
 #SBATCH --time=24:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=30
