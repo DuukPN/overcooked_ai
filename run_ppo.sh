462fa04ee679ffb74110d860b48eabe3792bccb5
Duuk Niemantsverdriet <D.P.Niemantsverdriet@student.tudelft.nl>
2023-06-18 14:40:06 +0200

    fewer ppo stuff

--- run_ppo.sh
+++ run_ppo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sbatch ~/overcooked_ai/src/human_aware_rl/ppo/job_ppo_1.sh
-sbatch ~/overcooked_ai/src/human_aware_rl/ppo/job_ppo_2.sh
+#sbatch ~/overcooked_ai/src/human_aware_rl/ppo/job_ppo_1.sh
+#sbatch ~/overcooked_ai/src/human_aware_rl/ppo/job_ppo_2.sh
 sbatch ~/overcooked_ai/src/human_aware_rl/ppo/job_ppo_3.sh
 sbatch ~/overcooked_ai/src/human_aware_rl/ppo/job_ppo_4.sh
 sbatch ~/overcooked_ai/src/human_aware_rl/ppo/job_ppo_5.sh
