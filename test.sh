db190a4bf5fe52d14b2a3e084b926c06fa1262b0
Duuk Niemantsverdriet <D.P.Niemantsverdriet@student.tudelft.nl>
2023-06-19 09:55:01 +0200

    test

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+#SBATCH --job-name="test"
+#SBATCH --partition=compute
+#SBATCH --time=00:01:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1GB
+#SBATCH --account=Education-EEMCS-Courses-CSE3000
+
+module load 2022r2
+module load miniconda3
+
+conda deactivate
+conda activate /scratch/dniemantsverdr/env
+
+srun python /scratch/dniemantsverdr/overcooked_ai/test.py
