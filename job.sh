9fcf9eda3f91725d1b1208eed7ffa708391d83fe
Duuk Niemantsverdriet <D.P.Niemantsverdriet@student.tudelft.nl>
2023-06-19 13:21:40 +0200

    more time

--- job.sh
+++ job.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH --job-name="bc_experiment"
 #SBATCH --partition=compute
-#SBATCH --time=04:00:00
+#SBATCH --time=08:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=40GB
