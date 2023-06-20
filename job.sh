cd8a870e6d05eb410052806cdc0f9266d6c82c67
Duuk Niemantsverdriet <D.P.Niemantsverdriet@student.tudelft.nl>
2023-06-20 12:58:40 +0200

    final push??

--- job.sh
+++ job.sh
@@ -4,8 +4,8 @@
 #SBATCH --partition=compute
 #SBATCH --time=08:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=40GB
+#SBATCH --cpus-per-task=5
+#SBATCH --mem-per-cpu=8GB
 #SBATCH --account=Education-EEMCS-Courses-CSE3000
 
 module load 2022r2
